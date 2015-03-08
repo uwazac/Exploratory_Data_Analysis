1639a2dfe202e73d481ad2609c4978bdf17b95dd
uwazac <gstephens@StephensFamilyMacBook.home>
2015-03-08 18:06:13 -0400

    updatted

--- plot1.R
+++ plot1.R
@@ -5,4 +5,5 @@ finalHpc<-cbind(finalHpc1, finalHpc2)
 test<-paste(finalHpc$Date, finalHpc$Time)
 test<-mutate(finalHpc, newDate=test)
 finalHpc<-test
+finalHpc$newDate<-strptime(finalHpc$newDate, format="%F %H:%M:%S")
 with(finalHpc, plot(newDate, Global_active_power, type="l", ylab="Global Active Power (kilowatts)", xlab=""))
