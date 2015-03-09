2c0c2a2ab841ff5fea2d44f301eec8bb633ece1d
uwazac <gstephens@StephensFamilyMacBook.home>
2015-03-08 23:05:15 -0400

    update

--- plot3.R
+++ plot3.R
@@ -1,9 +1,15 @@
-hpc<-read.table("household_power_consumption.txt", sep = ";", stringsAsFactors=FALSE)
-finalHpc1<-filter(hpc, Date=="2007-02-01")
-finalHpc2<-filter(hpc, Date=="2007-02-02")
+hpc<-read.table("household_power_consumption.txt", header=TRUE, sep = ";", stringsAsFactors=FALSE)
+newDates<-as.Date(hpc$Date, format  = "%d/%m/%Y")
+newHpc<-lapply(hpc[,3:9], function(x) as.numeric(x))
+newHpc<-as.data.frame(newHpc)
+hpc2<-cbind(hpc[,1:2], newHpc)
+hpc2$Date<-newDates
+finalHpc1<-filter(hpc2, Date=="2007-02-01")
+finalHpc2<-filter(hpc2, Date=="2007-02-02")
 finalHpc<-rbind(finalHpc1, finalHpc2)
-finalHpc$Date<-as.Date(finalHpc$Date)
-test<-paste(finalHpc$Date, finalHpc$Time)
+newDate<-as.Date(finalHpc$Date, format  = "%d/%m/%y")
+finalHpc$Date<-newDate
+test<-paste(as.character(finalHpc$Date), finalHpc$Time)
 test<-mutate(finalHpc, newDate=test)
 finalHpc<-test
 finalHpc$newDate<-strptime(finalHpc$newDate, format="%F %H:%M:%S")
