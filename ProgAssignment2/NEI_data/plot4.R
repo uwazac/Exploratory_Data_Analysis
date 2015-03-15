3544f699356b1b99313e7a0dd796b69bf38a1ef1
uwazac <geoffreystephens0@gmail.com>
2015-03-15 13:56:58 -0400

    updated

--- ProgAssignment2/NEI_data/plot4.R
+++ ProgAssignment2/NEI_data/plot4.R
@@ -5,5 +5,5 @@ coalRows<-grep("Coal", SCC$Short.Name)
 coal<-SCC[coalRows,]
 mergedCoal<-merge(NEI, coal, by ="SCC")
 totalUScoal<-summarize(group_by(mergedCoal, year), sum(Emissions))
-plot(totalUScoal[[1]], totalUScoal[[2]], xlab="Year", ylab="Emissions", main = " Emissions From Coal Combustion Sources",type="l", col="red")
+plot(totalUScoal[[1]], totalUScoal[[2]], xlab="Year", ylab="Emissions", main = " Emissions From Coal Combustion Sources",type="l", col="red", lwd=2)
 
--- ProgAssignment2/NEI_data/plot5.R
+++ ProgAssignment2/NEI_data/plot5.R
@@ -5,5 +5,5 @@ gas<-filter(SCC, SCC.Level.Two=="Highway Vehicles - Gasoline")
 mergedGas<-merge(NEI, gas, by="SCC")
 balt_gas<-filter(mergedGas, fips=="24510")
 total_balt_gas<-summarize(group_by(balt_gas, year), sum(Emissions))
-plot(total_balt_gas[[1]], total_balt_gas[[2]], xlab="Year", ylab="Emissions", main = " Emissions From Automobiles-Baltimore",type="l", col="red")
+plot(total_balt_gas[[1]], total_balt_gas[[2]], xlab="Year", ylab="Emissions", main = " Emissions From Automobiles-Baltimore",type="l", col="red", lwd=2)
 
