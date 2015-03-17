d697e2de4b5795114a111b56eb5f345eeac3123d
uwazac <gstephens@StephensFamilyMacBook.home>
2015-03-16 21:53:25 -0400

    updated

--- ProgAssignment2/NEI_data/plot5.R
+++ ProgAssignment2/NEI_data/plot5.R
@@ -1,9 +1,10 @@
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 library(dplyr)
-gas<-filter(SCC, SCC.Level.Two=="Highway Vehicles - Gasoline")
-mergedGas<-merge(NEI, gas, by="SCC")
-balt_gas<-filter(mergedGas, fips=="24510")
-total_balt_gas<-summarize(group_by(balt_gas, year), sum(Emissions))
-plot(total_balt_gas[[1]], total_balt_gas[[2]], xlab="Year", ylab="Emissions", main = " Emissions From Automobiles-Baltimore",type="l", col="red", lwd=2)
+vehicle<-grep("Veh", SCC$SCC.Level.Two)
+vehicle<-SCC[vehicle,]
+merged_veh<-merge(NEI, vehicle, by="SCC")
+balt_veh<-filter(merged_veh, fips=="24510")
+total_balt_veh<-summarize(group_by(balt_veh, year), sum(Emissions))
+plot(total_balt_veh[[1]], total_balt_veh[[2]], xlab="Year", ylab="Emissions", main = " Emissions From Motor Vehicles-Baltimore",type="l", col="red", lwd=2)
 
