e15786c9c04c330de07ddecadfe4a34029dcd6ca
uwazac <gstephens@StephensFamilyMacBook.home>
2015-03-20 21:49:15 -0400

    updates

--- ProgAssignment2/NEI_data/plot1.R
+++ ProgAssignment2/NEI_data/plot1.R
@@ -1,8 +1,17 @@
+# Read in the data files
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
-library(dplyr)
-emission_by_year<-summarize(group_by(NEI, year), sum(Emissions))
-plot(emission_by_year[[1]], emission_by_year[[2]], xlab="Year", ylab="PM2.5 emissions", main = "US Total PM2.5 Emissions",type="l", col="red", lwd=2)
 
-#Total emissions have shown a decreasing trend over the time period evaluated 
+# Load the 'dplyr' functions
+library(dplyr)
+
+# Use the summarize function from dplyr to find the total emmisions in different years
+emission_by_year<-summarize(group_by(NEI, year), sum(Emissions))
+
+# Use the plot function to display the data
+plot(emission_by_year[[1]], emission_by_year[[2]], xlab="Year", ylab="PM2.5 emissions", main = "US Total PM2.5 Emissions",type="l", col="red", lwd=2, xaxt="n")
+
+# Change the x-axis labels to align with the data time points
+axis(1,at=c(1999, 2002, 2005, 2008))
+# Total emissions have shown a decreasing trend over the time period evaluated 
 
